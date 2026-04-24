38a699b50e04c8431d3728a27526de185fcbb644
Raihan Yusuf Sa'ari <raihanwashere@gmail.com>
2026-04-25 02:44:33 +0700

    db fix

--- config/database.php
+++ config/database.php
@@ -60,9 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                1007 => true,
-                1014 => false,
+                PDO::MYSQL_ATTR_SSL_CA => null,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
